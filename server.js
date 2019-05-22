76b462a7de7d0fc50c933975c7308a2100ce7e71
vikash <vikashraj144@gmail.com>
2019-05-22 20:50:14 +0530

    body parser added

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const app  = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
 var server  = http.createServer(app);
 server.listen(port, () => { console.log(`Server is listening on port => ${port}`); })
 
