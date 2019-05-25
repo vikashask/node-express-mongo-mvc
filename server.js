b514720ccd7bf1fd89852949a3fd980e8a8e4271
vikash <vikashraj144@gmail.com>
2019-05-25 16:41:48 +0530

    cors and multer added

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const express  = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const app  = express();
+const cors = require('cors');
+const multer = require('multer');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(multer().any());
+app.use(cors()); //CoRS
 
 
 var server  = http.createServer(app);
-server.listen(port, () => { console.log(`Server is listening on port => ${port}`); })
 
+server.listen(port, () => { console.log(`Server is listening on port => ${port}`); })
