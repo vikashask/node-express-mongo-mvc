d6e0d4c8e7ea8d94991f5f9c49e6ee6ae884bb42
vikash <vikashraj144@gmail.com>
2019-05-27 20:15:23 +0530

    creating object of express

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const dotenv = require('dotenv').config({path:'./.env'})
 const port = process.env.PORT || 8080;
-const express  = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const app  = express();
 const cors = require('cors');
 const multer = require('multer');
+const express  = require('express');
+const app  = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
 app.use(multer().any());
-app.use(cors()); //CoRS
+app.use(cors()); //enable cores
 
+// adding mongo connection
+require('./config/dbConnection')(app);
 
+// creating server
 var server  = http.createServer(app);
 
 server.listen(port, () => { console.log(`Server is listening on port => ${port}`); })
