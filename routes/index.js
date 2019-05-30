6bfcd577189b42ed3c0bb078494592014e1b3ed7
vikash <vikashraj144@gmail.com>
2019-05-30 22:53:20 +0530

    route up

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const userRoutes = require('./user/user.routes');
+const bookRoutes = require('./book/book.routes');
 
-router.use('/api/user', userRoutes);
+router.use('/user', userRoutes);
+router.use('/book', bookRoutes);
 module.exports = router;
