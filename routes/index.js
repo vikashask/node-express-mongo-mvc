c85af23b3c7010693c59b1a3152fc8c7e80cff72
vikash <vikashraj144@gmail.com>
2019-05-30 23:06:47 +0530

    author route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const userRoutes = require('./user/user.routes');
 const bookRoutes = require('./book/book.routes');
+const authorsRoutes = require('./author/author.routes');
 
 router.use('/user', userRoutes);
 router.use('/book', bookRoutes);
+router.use('/authors', authorsRoutes);
 module.exports = router;
