75b825489a812f7ba801edfce6838cf326ccf085
vikash <vikashraj144@gmail.com>
2019-06-01 17:46:57 +0530

    up

--- routes/bookinstance/bookinstance.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const bookinstanceController = require('../../controllers/bookinstanceController');
-router.get('/', bookinstanceController.bookinstance_list);
-
-module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,9 @@ const userRoutes = require('./user/user.routes');
 const bookRoutes = require('./book/book.routes');
 const authorsRoutes = require('./author/author.routes');
 const genresRoutes = require('./genres/genres.routes');
-const bookinstanceRoute = require('./bookinstance/bookinstance.routes');
 
 router.use('/user', userRoutes);
 router.use('/book', bookRoutes);
 router.use('/authors', authorsRoutes);
 router.use('/genres', genresRoutes);
-router.use('/bookinstance', bookinstanceRoute);
 module.exports = router;
