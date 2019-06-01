156aafe23554f09168ae8b2c33e3eda92ffcd7c4
vikash <vikashraj144@gmail.com>
2019-06-01 17:28:43 +0530

    genres

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router();
 const userRoutes = require('./user/user.routes');
 const bookRoutes = require('./book/book.routes');
 const authorsRoutes = require('./author/author.routes');
+const genresRoutes = require('./genres/genres.routes');
 
 router.use('/user', userRoutes);
 router.use('/book', bookRoutes);
 router.use('/authors', authorsRoutes);
+router.use('/genres', genresRoutes);
 module.exports = router;
