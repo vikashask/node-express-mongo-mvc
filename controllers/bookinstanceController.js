7b1187bb543ea78a8c101c4b26bb0ea5d96ad352
vikash <vikashraj144@gmail.com>
2019-06-02 17:09:42 +0530

    bookintance

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,6 +1,7 @@
 var Book = require('../models/book');
 var Author = require('../models/author');
 var Genre = require('../models/genre');
+var BookInstance = require('../models/bookinstance');
 
 // Display list of all bookinstance_list
 exports.bookinstance_list = function (req, res, next) {
