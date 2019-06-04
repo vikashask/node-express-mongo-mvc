8e5739e29df82c12f99927fb22e3c11853bc3b45
vikash <vikashraj144@gmail.com>
2019-06-04 23:35:05 +0530

    create author update in controller

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,5 @@
 var Book = require('../models/book');
 var Author = require('../models/author');
-var Genre = require('../models/genre');
 
 // Display list of all books
 exports.author_list = function (req, res, next) {
@@ -10,4 +9,22 @@ exports.author_list = function (req, res, next) {
       // succesful rendering
       res.send({list_authors})
     });
+};
+// Display list of all books
+exports.createAuthor = function (req, res, next) {
+  var author = new Author(
+    {
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death
+  });
+  
+  author.save(function (err) {
+    if (err) {
+      console.log('error',err);
+      return next(err);      
+    }
+    res.send({mes:'author created'})
+  });
 };
