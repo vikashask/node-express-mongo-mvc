56d08c0ed3f8ecd940fc19eb65dc6dc7081c8fd7
vikash <vikashraj144@gmail.com>
2021-04-12 21:27:40 +0530

    add book api added

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,13 +1,27 @@
-var Book = require('../models/book');
-var Author = require('../models/author');
-var Genre = require('../models/genre');
+var Book = require("../models/book");
+var Author = require("../models/author");
+var Genre = require("../models/genre");
 
 // Display list of all books
 exports.book_list = function (req, res, next) {
-  Book.find({}, 'title author ')
-    .populate('author')
+  Book.find({}, "title author ")
+    .populate("author")
     .exec(function (err, book_list) {
       if (err) return next(err);
-      res.send({book_list})
+      res.send({ book_list });
     });
+};
+
+exports.addBook = (req, res, next) => {
+  let book = new Book({
+    title: req.body.title,
+    author: req.body.author,
+    summary: req.body.summary,
+    isbn: req.body.isbn,
+    genre: req.body.genre,
+  });
+  book.save((err) => {
+    if (err) return next(err);
+    res.send("book save");
+  });
 };
