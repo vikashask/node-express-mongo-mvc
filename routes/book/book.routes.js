c125d54688ec072a2169cf8c5ad3737aa5fbc212
vikash <vikashraj144@gmail.com>
2021-04-12 21:27:58 +0530

    add book route added

--- routes/book/book.routes.js
+++ routes/book/book.routes.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const bookController = require('../../controllers/bookController');
-router.get('/', bookController.book_list);
+const bookController = require("../../controllers/bookController");
+router.get("/", bookController.book_list);
+router.post("/", bookController.addBook);
 
 module.exports = router;
