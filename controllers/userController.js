60c78d3984b57a99c3a23f9b600f18c2bf09a99d
vikash <vikashraj144@gmail.com>
2021-04-14 07:21:26 +0530

    user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,11 @@
-var Book = require('../models/book');
-var Author = require('../models/author');
-var Genre = require('../models/genre');
+var User = require("../models/user");
 
 // Display list of all books
-exports.book_list = function (req, res, next) {
-  Book.find({}, 'title author ')
-    .populate('author')
+exports.user_list = function (req, res, next) {
+  Book.find({}, "title author ")
+    .populate("author")
     .exec(function (err, list_books) {
       if (err) return next(err);
-      res.render('book_list', { title: 'Book List', book_list: list_books });
+      res.render("book_list", { title: "Book List", book_list: list_books });
     });
 };
