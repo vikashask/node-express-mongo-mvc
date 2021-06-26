4acb8af458cb49ea0b1167ac1f04df701a1405f5
vikash <vikashraj144@gmail.com>
2021-06-26 09:35:33 +0530

    cluster added in server to enable multi core

--- server.js
+++ server.js
@@ -1,25 +1,48 @@
-const dotenv = require('dotenv').config({path:'./.env'})
+const dotenv = require("dotenv").config({ path: "./.env" });
 const port = process.env.PORT || 8080;
-const http = require('http');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const multer = require('multer');
-const express  = require('express');
-const app  = express();
+const http = require("http");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const multer = require("multer");
+const express = require("express");
+const app = express();
+const cluster = require("cluster");
+const os = require("os");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(multer().any());
 app.use(cors()); //enable cores
 
 // adding mongo connection
-require('./config/dbCon')(app);
+require("./config/dbCon")(app);
 
-var routes = require('./routes');
-app.use('/', routes)
+const numCpu = os.cpus().length;
+// cluster integration
+app.get("/", (req, res) => {
+  res.send(`OKkkkk--- ${process.pid}`);
+  cluster.worker.kill();
+});
 
+var routes = require("./routes");
+app.use("/", routes);
 // creating server
-var server  = http.createServer(app);
+var server = http.createServer(app);
 
-server.listen(port, () => { console.log(`Server is listening on port => ${port}`); })
+// server.listen(port, () => {
+//   console.log(`Server is listening on port => ${port}`);
+// });
+
+if (cluster.isMaster) {
+  for (let index = 0; index < numCpu; index++) {
+    cluster.fork();
+  }
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  server.listen(port, () => {
+    console.log(`Server is listening pid ${process.pid} on port => ${port}`);
+  });
+}
